c3c33c3e61b409f72e4cf1aea5f5a7206774c41e
Deceitful <gfelipe0555@gmail.com>
2025-02-18 02:18:18 -0300

    First steps in Java with Spring boot 3.4.0

---
+++ restful-with-springboot3/src/main/java/br/com/deceitful1/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.deceitful1.model;
+
+public record Greeting(long id, String content)
+{
+}
---
+++ restful-with-springboot3/src/main/java/br/com/deceitful1/restfulwithspringboot3/RestfulWithSpringboot3Application.java
@@ -0,0 +1,13 @@
+package br.com.deceitful1.restfulwithspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "br.com.deceitful1")
+public class RestfulWithSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWithSpringboot3Application.class, args);
+	}
+
+}
