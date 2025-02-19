b57fc9b02d7cf3c58dac1d2d3e5f09101fa9e2f0
Deceitful <gfelipe0555@gmail.com>
2025-02-19 13:06:16 -0300

    refactoring my code

--- restful-with-springboot3/src/main/java/br/com/deceitful1/controllers/MathController.java
+++ restful-with-springboot3/src/main/java/br/com/deceitful1/controllers/MathController.java
@@ -1,11 +1,9 @@
 package br.com.deceitful1.controllers;
 
-import br.com.deceitful1.controllers.exceptions.UnsupportedMathOperationException;
+import br.com.deceitful1.models.MathOperations;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/math")
@@ -22,91 +20,37 @@ public class MathController
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
     {
-        if (numberOne == null || numberTwo == null) {
-            throw new UnsupportedMathOperationException("Numbers must not be null");
-        }
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-        }
-
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return MathOperations.sum(numberOne, numberTwo);
     }
 
     @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo)
     {
-        if (numberOne == null || numberTwo == null)
-            throw new UnsupportedMathOperationException("Numbers must not be null");
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return MathOperations.subtraction(numberOne, numberTwo);
     }
 
     @RequestMapping("/divide/{numberOne}/{numberTwo}")
     public Double divide(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo)
     {
-        if (numberOne == null || numberTwo == null)
-            throw new UnsupportedMathOperationException("Numbers must not be null");
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return MathOperations.divide(numberOne, numberTwo);
     }
 
     @RequestMapping("/multiply/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo)
     {
-        if (numberOne == null || numberTwo == null)
-            throw new UnsupportedMathOperationException("Numbers must not be null");
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
-
+        return MathOperations.multiplication(numberOne, numberTwo);
     }
 
     @RequestMapping("/average/{numbers}")
     public Double average(@PathVariable String numbers)
     {
-        if (numbers == null) throw new UnsupportedMathOperationException("Numbers must not be null");
-        if (!isNumeric(numbers))
-            throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-
-        double[] values = Arrays.stream(numbers.split(","))
-                .mapToDouble(Double::parseDouble)
-                .toArray();
-
-        return Arrays.stream(values).average().orElse(0);
+        return MathOperations.average(numbers);
     }
 
     @RequestMapping("/squareroot/{number}")
     public Double squareRoot(@PathVariable(value = "number") String number)
     {
-        if (number == null) throw new UnsupportedMathOperationException("Numbers must not be null");
-        if (!isNumeric(number)) throw new UnsupportedMathOperationException("The variable must only contains numbers!");
-        return Math.sqrt(convertToDouble(number));
-    }
-
-
-    private Double convertToDouble(String strNumber)
-    {
-        if (strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedMathOperationException("The variable must not be null or empty!");
-        String number = strNumber.replaceAll(",", ".");
-
-
-        return Double.parseDouble(number);
-
-    }
-
-
-    private boolean isNumeric(String strNumber)
-    {
-        if (strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replaceAll(",", ".");
-        if (number.matches("[0-9]") || number.matches("[-+]?[0-9]*\\.?[0-9]+"))
-            return true;
-
-
-        return false;
+        return MathOperations.squareRoot(number);
     }
 
 
--- restful-with-springboot3/src/main/java/br/com/deceitful1/models/Math.java
+++
@@ -1,5 +0,0 @@
-package br.com.deceitful1.models;
-
-public record Math( double num2)
-{
-}
